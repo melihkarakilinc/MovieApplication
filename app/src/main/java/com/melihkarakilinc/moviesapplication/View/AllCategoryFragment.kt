10b8d619f672d6735dd87474eff72c43daa93413
melihkarakilinc <57638333+melihkarakilinc@users.noreply.github.com>
2022-03-03 23:14:18 +0300

    #Get Movie Data at Fragment and Add Navigation Comp.

---
+++ app/src/main/java/com/melihkarakilinc/moviesapplication/View/AllCategoryFragment.kt
@@ -0,0 +1,55 @@
+package com.melihkarakilinc.moviesapplication.View
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.melihkarakilinc.moviesapplication.Status
+import com.melihkarakilinc.moviesapplication.ViewModel.MainViewModel
+import com.melihkarakilinc.moviesapplication.databinding.FragmentAllCategoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class AllCategoryFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentAllCategoryBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAllCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.getMovie(28)
+        lifecycleScope.launch {
+            viewModel.movieState.collect {
+                when (it.status) {
+                    Status.LOADING -> {
+                        //Show Progressbar
+                    }
+                    Status.SUCCESS -> {
+                        Toast.makeText(
+                            requireContext(),
+                            it.data?.results.toString(),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    else -> {
+                        Log.e("MainActivity", it.message.toString())
+                    }
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/melihkarakilinc/moviesapplication/View/MainActivity.kt
+++ app/src/main/java/com/melihkarakilinc/moviesapplication/View/MainActivity.kt
@@ -1,45 +1,14 @@
 package com.melihkarakilinc.moviesapplication.View
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.melihkarakilinc.moviesapplication.Status
-import com.melihkarakilinc.moviesapplication.ViewModel.MainViewModel
 import com.melihkarakilinc.moviesapplication.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
-        viewModel.getMovie(28)
-        lifecycleScope.launch {
-            viewModel.movieState.collect {
-                when (it.status) {
-                    Status.LOADING -> {
-                        //Show Progressbar
-                    }
-                    Status.SUCCESS -> {
-                        Toast.makeText(
-                            this@MainActivity,
-                            it.data?.results.toString(),
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    else -> {
-                        Log.e("MainActivity", it.message.toString())
-                    }
-                }
-            }
-        }
     }
 }
